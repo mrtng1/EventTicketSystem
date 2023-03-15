308a00c63a9eeaa2404f63f4bfa5fa6566525375
mrtng1 <mrtngrulyo@gmail.com>
2023-03-15 11:28:47 +0100

    code basics added

---
+++ src/dal/DbConnect.java
@@ -0,0 +1,33 @@
+package dal;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DbConnect {
+    private SQLServerDataSource dataSource;
+
+    public DbConnect(){
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.34");
+        dataSource.setDatabaseName("CSe2022B_e_17_TicketSystem");
+        dataSource.setUser("CSe2022B_e_17");
+        dataSource.setPassword("CSe2022BE17#");
+        dataSource.setPortNumber(1433);
+        dataSource.setTrustServerCertificate(true);
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DbConnect dataBaseConnection = new DbConnect();
+        Connection connection = dataBaseConnection.getConnection();
+
+        System.out.println("Database connected" + !connection.isClosed());
+        connection.close();
+    }
+}
---
+++ src/gui/controller/MainWindowController.java
@@ -0,0 +1,16 @@
+package gui.controller;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainWindowController {
+
+    @FXML
+    private MFXButton button;
+
+    @FXML
+    public void initialize() {
+
+    }
+}

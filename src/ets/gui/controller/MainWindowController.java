dfeb47cfa5c214c6dfb7f141596043b511586244
mrtng1 <mrtngrulyo@gmail.com>
2023-03-19 01:04:09 +0100

    updated event card

--- src/ets/gui/controller/MainWindowController.java
+++ src/ets/gui/controller/MainWindowController.java
@@ -3,6 +3,7 @@ package ets.gui.controller;
 import javafx.animation.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -16,6 +17,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -32,7 +34,7 @@ public class MainWindowController implements Initializable {
         double startValue = scrollPane.getVvalue();
         double endValue = startValue + 1;
         Timeline timeline = new Timeline(
-                new KeyFrame(Duration.seconds(1),
+                new KeyFrame(Duration.seconds(0.5),
                         new KeyValue(scrollPane.vvalueProperty(), endValue, Interpolator.EASE_BOTH)
                 )
         );
@@ -41,17 +43,16 @@ public class MainWindowController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        populateGridPane();
+        try {
+            populateGridPane();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void populateGridPane() {
+    private void populateGridPane() throws IOException {
         int numRows = 5;
         int numColumns = 3;
-        String imagePath = "/ets/gui/view/images/questionmark.png";
-        double imageWidth = 1;
-        double imageHeight = 1;
-        double imageViewWidth = 100;
-        double imageViewHeight = imageViewWidth * (imageHeight / imageWidth);
         String borderColor = "#000000"; // You can change this to any color you like (e.g., #FF0000 for red)
         double borderWidth = 1.0; // Adjust the border width as needed
 
@@ -59,37 +60,16 @@ public class MainWindowController implements Initializable {
             for (int col = 0; col < numColumns; col++) {
                 Pane pane = new Pane();
 
-                Image image = new Image(imagePath);
-                ImageView imageView = new ImageView(image);
-                imageView.setFitWidth(imageViewWidth);
-                imageView.setFitHeight(imageViewHeight);
-                imageView.setPreserveRatio(true);
-                imageView.setSmooth(true);
+                // Load the FXML file
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ets/gui/view/eventCard.fxml"));
+                Pane contentPane = fxmlLoader.load();
 
-                // Create the label
-                Label label = new Label("Empty event");
+                // Add the loaded content to the pane
+                pane.getChildren().add(contentPane);
 
-                // Create the button and style it
-                Button viewBtn = new Button("View");
-                viewBtn.setStyle("-fx-background-radius: 20;");
-
-                // Create an HBox for the ImageView and label, adjust spacing
-                HBox imageLabelHBox = new HBox(10, imageView, label);
-                imageLabelHBox.setAlignment(Pos.TOP_CENTER);
-                imageLabelHBox.setSpacing(2);
-
-                // Wrap the button in an HBox and set its alignment
-                HBox buttonHBox = new HBox(viewBtn);
-                buttonHBox.setAlignment(Pos.CENTER_RIGHT);
-
-                // Create a VBox for the HBox and button, adjust spacing
-                VBox contentVBox = new VBox(2, imageLabelHBox, buttonHBox);
-                contentVBox.setAlignment(Pos.TOP_CENTER);
-                contentVBox.setPrefSize(pane.getPrefWidth(), pane.getPrefHeight());
-                contentVBox.setSpacing(-20);
-
-                pane.getChildren().add(contentVBox);
+                // Apply the border outline
                 pane.setStyle("-fx-border-color: " + borderColor + "; -fx-border-width: " + borderWidth + ";");
+
                 eventPane.add(pane, col, row);
             }
         }

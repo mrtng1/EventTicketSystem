b15259254848f5ef1c57c40d0b400ae1c0d22143
Tomáš Dračka <tomidracka@gmail.com>
2023-03-23 10:40:22 +0100

    coordinator_window.fxml

---
+++ src/ets/gui/controller/CoordinatorWindowController.java
@@ -0,0 +1,19 @@
+package ets.gui.controller;
+
+// imports
+import javafx.fxml.Initializable;
+
+// java imports
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author tomdra01, mrtng1
+ */
+public class CoordinatorWindowController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
